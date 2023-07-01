d83a466c7b7085dcb1aab572c058884f40f3175d
Jonas Nygren <jonas_nygren30@hotmail.com>
2023-07-01 17:54:07 +0200

    Update ssh-to-mikrotik.py

--- ssh-to-mikrotik.py
+++ ssh-to-mikrotik.py
@@ -1,1 +1,20 @@
+from netmiko import ConnectHandler
+
+mikrotik = {
+    'device_type': 'mikrotik_routeros',
+    'ip': '192.168.1.220',
+    'username': 'admin',
+    'password': 'password',
+    'port': 22,  # SSH port (default: 22)
+}
+
+# Establish SSH connection
+net_connect = ConnectHandler(**mikrotik)
+
+# Send command and retrieve output
+output = net_connect.send_command('interface print')
+print(output)
+
+# Disconnect from the device
+net_connect.disconnect()
 
